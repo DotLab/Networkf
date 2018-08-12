eb15dd15c619bf07eef919b9c580ed33b3f2cc61
Kailang Fu <kailangfu@gmail.com>
2018-08-12 00:52:14 -0500

    Update BitHelper

--- Networkf/BitHelper.cs
+++ Networkf/BitHelper.cs
@@ -1,15 +1,23 @@
 ﻿namespace Networkf {
 	public static class BitHelper {
-		public static byte ReadByte(byte[] buf, ref int i) {
+		public static byte ReadUInt8(byte[] buf, ref int i) {
 			return buf[i++];
 		}
 
+		public static short ReadInt16(byte[] buf, ref int i) {
+			return (short)(buf[i++] | (sbyte)buf[i++] << 8);
+		}
+
+		public static ushort ReadUInt16(byte[] buf, ref int i) {
+			return (ushort)(buf[i++] | buf[i++] << 8);
+		}
+
 		public static int ReadInt32(byte[] buf, ref int i) {
-			return (int)buf[i++] | buf[i++] << 8 | buf[i++] << 16 | (sbyte)buf[i++] << 24;
+			return buf[i++] | buf[i++] << 8 | buf[i++] << 16 | (sbyte)buf[i++] << 24;
 		}
 
 		public static uint ReadUInt32(byte[] buf, ref int i) {
-			return (uint)(buf[i++] | buf[i++] << 8 | buf[i++] << 16 | buf[i++] << 24);
+			return (uint)buf[i++] | (uint)buf[i++] << 8 | (uint)buf[i++] << 16 | (uint)buf[i++] << 24;
 		}
 
 		public static string ReadString(byte[] buf, ref int i, int len) {
@@ -18,14 +26,28 @@
 			return str;
 		}
 
-		public static void WriteInt32 (byte[] buf, ref int i, int val) {
+		public static void WriteUInt8(byte[] buf, ref int i, byte val) {
+			buf[i++] = val;
+		}
+
+		public static void WriteInt16(byte[] buf, ref int i, short val) {
+			buf[i++] = (byte)(val & 0xFF);
+			buf[i++] = (byte)((val >> 8) & 0xFF);
+		}
+
+		public static void WriteUInt16(byte[] buf, ref int i, ushort val) {
+			buf[i++] = (byte)(val & 0xFF);
+			buf[i++] = (byte)((val >> 8) & 0xFF);
+		}
+
+		public static void WriteInt32(byte[] buf, ref int i, int val) {
 			buf[i++] = (byte)(val & 0xFF);
 			buf[i++] = (byte)((val >> 8) & 0xFF);
 			buf[i++] = (byte)((val >> 16) & 0xFF);
 			buf[i++] = (byte)((val >> 24) & 0xFF);
 		}
 
-		public static void WriteUInt32 (byte[] buf, ref int i, uint val) {
+		public static void WriteUInt32(byte[] buf, ref int i, uint val) {
 			buf[i++] = (byte)(val & 0xFF);
 			buf[i++] = (byte)((val >> 8) & 0xFF);
 			buf[i++] = (byte)((val >> 16) & 0xFF);
